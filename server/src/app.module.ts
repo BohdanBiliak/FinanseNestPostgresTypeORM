1dca5ec301ffac755541c3223ef82a450c374629
Bohdan Biliak <arheroha@gmasil.com>
2025-06-01 15:12:01 +0100

    add user auth trans category modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {UserModule} from "./user/user.module";
+import {CategoryModule} from "./category/category.module";
+import { AuthModule } from './auth/auth.module';
+import { ResourceModule } from './transaction/resource/resource.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CategoryModule, AuthModule, ResourceModule],
   controllers: [AppController],
   providers: [AppService],
 })
