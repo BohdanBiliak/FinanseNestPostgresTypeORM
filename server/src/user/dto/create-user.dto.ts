1c501d6b02d9a3a170a7c0ef801136fb21e75c00
Bohdan Biliak <arheroha@gmasil.com>
2025-06-04 13:40:18 +0100

    add login controllers

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import {IsEmail, MaxLength, MinLength} from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+    @MinLength(6, {message: 'passord must be more then 6 symbols'})
+    @MaxLength(20)
+    password: string;
+}
